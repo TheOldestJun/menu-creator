c167f6ec977a3b439550890d94ae84f0b0fb2b7a
Stanislav Pukha <s.v.pukha@gmail.com>
2024-05-25 07:55:45 +0300

    admin panel init

---
+++ app/admin/panel/page.js
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
+
+export default function Page() {
+  const tabsData = [
+    {
+      label: "Создать блюдо",
+      value: "create-recipe",
+    },
+    {
+      label: "Создать продукт",
+      value: "create-product",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col items-center">
+      <Card className="w-[98vw]">
+        <CardBody>
+          <Typography
+            variant="h5"
+            color="blue-gray"
+            className="text-center mb-3"
+          >
+            Панель администратора
+          </Typography>
+          <Tabs value={tabsData[0].value}>
+            <TabsHeader>
+              {tabsData.map(({ label, value }) => (
+                <Tab key={value} value={value}>
+                  {label}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody>
+              <TabPanel value="create-recipe">Create recipe</TabPanel>
+              <TabPanel value="create-product">Create product</TabPanel>
+            </TabsBody>
+          </Tabs>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
