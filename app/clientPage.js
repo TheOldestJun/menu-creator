40904a2f2291e4b20e9f6ab336c0f7b06c589eac
Stanislav Pukha <s.v.pukha@gmail.com>
2024-05-28 19:09:36 +0300

    animations init

--- app/clientPage.js
+++ app/clientPage.js
@@ -1,7 +1,26 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const ClientPage = () => {
+  const component = useRef(null);
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      ".menu-animation",
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        duration: 2,
+        x: 0,
+        ease: "power4.out",
+      }
+    );
+  }, []);
   return (
     <>
       <div className="fixed inset-0 -z-10">
@@ -13,13 +32,13 @@ const ClientPage = () => {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="fixed inset-0 -z-8 mt-[90px] ml-[185px]">
+      <div className="">
         <Image
           src="/dishes/menu.webp"
           alt="background image"
           width="450"
           height="640"
-          className="rounded-lg"
+          className="rounded-lg opacity-0 menu-animation fixed -z-8 mt-2 md:ml-[185px] mx-auto "
         />
       </div>
     </>
